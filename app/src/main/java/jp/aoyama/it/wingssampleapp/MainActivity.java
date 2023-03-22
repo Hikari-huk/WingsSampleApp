9974b07dade2cef4f750965c9dbc288f232aee57
hikari <ghikariao974@gmail.com>
2023-03-22 14:57:09 +0900

    [add] アダプターによるメニュー追加

--- app/src/main/java/jp/aoyama/it/wingssampleapp/MainActivity.java
+++ app/src/main/java/jp/aoyama/it/wingssampleapp/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +20,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // ListViewオブジェクトを取得。
         ListView lvMenu = findViewById(R.id.lvMenu);
-        // ListViewにリスナを設定。
+        // リストビューに表示するリストデータ用Listオブジェクトを作成。
+        List<String> menuList = new ArrayList<>();
+        // リストデータの登録。
+        menuList.add("から揚げ定食");
+        menuList.add("ハンバーグ定食");
+        menuList.add("生姜焼き定食");
+        menuList.add("ステーキ定食");
+        menuList.add("野菜炒め定食");
+        menuList.add("とんかつ定食");
+        menuList.add("ミンチかつ定食");
+        menuList.add("チキンカツ定食");
+        menuList.add("コロッケ定食");
+        menuList.add("回鍋肉定食");
+        menuList.add("麻婆豆腐定食");
+        menuList.add("青椒肉絲定食");
+        menuList.add("焼き魚定食");
+        menuList.add("焼肉定食");
+        // アダプタオブジェクトを生成。
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, menuList);
+        // リストビューにアダプタオブジェクトを設定。
+        lvMenu.setAdapter(adapter);
+        // リストビューにリスナを設定。
         lvMenu.setOnItemClickListener(new ListItemClickListener());
     }
 
