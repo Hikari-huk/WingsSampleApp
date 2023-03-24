c2f7c824a39425aec37359389fe84fb0c9f78c1f
hikari <ghikariao974@gmail.com>
2023-03-25 01:41:18 +0900

    [add] 画面部品完成

--- app/src/main/java/jp/aoyama/it/wingssampleapp/MainActivity.java
+++ app/src/main/java/jp/aoyama/it/wingssampleapp/MainActivity.java
@@ -3,13 +3,50 @@ package jp.aoyama.it.wingssampleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int _cocktailId = -1;
+
+    private String _cocktailName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("10sec");
+
+        ListView lvCocktail = findViewById(R.id.lvCocktail);
+        lvCocktail.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    public void onSaveButtonClick(View view){
+        EditText etNote = findViewById(R.id.etNote);
+
+        etNote.setText("");
+
+        TextView tvCocktailName = findViewById(R.id.tvCocktailName);
+        tvCocktailName.setText(getString(R.string.tv_name));
+
+        Button btnSave = findViewById(R.id.btnSave);
+        btnSave.setEnabled(false);
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+            _cocktailId = position;
+            _cocktailName = (String)parent.getItemAtPosition(position);
+
+            TextView tvCocktailName = findViewById(R.id.tvCocktailName);
+            tvCocktailName.setText(_cocktailName);
+            Button btnSave = findViewById(R.id.btnSave);
+            btnSave.setEnabled(true);
+        }
     }
 }
