a4c1a5cf9d3ee6fa57343c43df61da66a8c5147c
hikari <ghikariao974@gmail.com>
2023-04-02 03:29:57 +0900

    [add] Serviceを追加

--- app/src/main/java/jp/aoyama/it/wingssampleapp/MainActivity.java
+++ app/src/main/java/jp/aoyama/it/wingssampleapp/MainActivity.java
@@ -2,10 +2,12 @@ package jp.aoyama.it.wingssampleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,14 +21,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("13sec");
-        System.out.println("12sec");
-        System.out.println("11章");
-        System.out.println("10sec");
-        System.out.println("5章");
-        System.out.println("7章");
 
     }
 
+    public void onPlayButtonClick(View view){
+        Intent intent = new Intent(MainActivity.this, SoundManageService.class);
+
+        startService(intent);
+
+        Button btPlay = findViewById(R.id.btPlay);
+        Button btStop = findViewById(R.id.btStop);
+
+        btPlay.setEnabled(false);
+        btStop.setEnabled(true);
+    }
+
+    public void onStopButtonClick(View view){
+        Intent intent = new Intent(MainActivity.this, SoundManageService.class);
+
+        stopService(intent);
+
+        Button btPlay = findViewById(R.id.btPlay);
+        Button btStop = findViewById(R.id.btStop);
+        btPlay.setEnabled(true);
+        btStop.setEnabled(false);
+    }
 }
 
