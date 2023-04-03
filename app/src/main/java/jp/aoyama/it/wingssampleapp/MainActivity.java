c77b2ceec0d92b0ca5c934a33324fabf7e927561
hikari <ghikariao974@gmail.com>
2023-04-03 16:59:14 +0900

    [add] 暗黙インテントと地図を開く処理

--- app/src/main/java/jp/aoyama/it/wingssampleapp/MainActivity.java
+++ app/src/main/java/jp/aoyama/it/wingssampleapp/MainActivity.java
@@ -2,7 +2,15 @@ package jp.aoyama.it.wingssampleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("14sec");
+    }
+
+    public void onMapSearchButtonClick(View view){
+        EditText etSearchWord = findViewById(R.id.etSearchWord);
+        String searchWord = etSearchWord.getText().toString();
+
+        try {
+            searchWord = URLEncoder.encode(searchWord, "UTF-8");
+
+            String uriStr = "geo:0,0?q=" + searchWord;
+
+            Uri uri = Uri.parse(uriStr);
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+            startActivity(intent);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("MainActivity", "検索キーワード変換失敗", e);
+        }
     }
 }
